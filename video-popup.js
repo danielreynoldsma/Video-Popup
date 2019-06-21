118d512e47d717c5a1f9730e7df7d8b286c064a5
camelCaseCo <danieljreyn@gmail.com>
2019-06-21 12:08:22 -0400

    added example.html and example.mp4. added main functionality to main .js file.
    add css next

--- video-popup.js
+++ video-popup.js
@@ -1,10 +1,49 @@
-VideoPopup('C:\\Users\\DJRco_000\\Videos\\Roblox\\robloxapp-20170828-1334197.wmv');
+var isVideoDisplayed = false;
+const videoTemplate = 
+"<video id='popupvideo' width='320' height='240 controls>" +
+"<source src='vidsrc' type'video/mp4'>" +
+"</video>";
 
-function VideoPopup(url, skip) {
-    if(skip === undefined) {
-        skip = 'false';
+function VideoPopup(url, size, shouldSkip = false) {
+    if(typeof size === 'string') {
+        if(size === 'small' || size === 'medium' || size === 'large') {
+            if(size === 'small') {
+                size = {width:320, height:240};
+            }else if(size === 'medium') {
+                size = {width:640, height:480};
+            }else if(size === 'large') {
+                size = {width:1024, height:768};
+            }
+        } else {
+            console.error('VideoPopup size is a string, but is not "small", "medium", or "large".');
+        }
     }
-    window.alert(skip);
+    if(typeof size === 'object') {
+        if('width' in size) {
+            var hasWidth = true;
+        } else {
+            var hasWidth = false;
+        }
+        if('height' in size) {
+            var hasHeight = true;
+        } else {
+            var hasHeight = false;
+        }
+        if(!hasHeight && !hasHeight) {
+            console.error('VideoPopup missing width and height parameters in size.');
+        }
+        if(!hasHeight) {
+            console.error('VideoPopup missing height parameter in size.');
+        }
+        if(!hasWidth) {
+            console.error('VideoPopup missing width parameter in size.');
+        }
+        if(hasHeight && hasWidth) {
+            size = {width:size.width, height:size.height};
+            document.body.innerHTML += videoTemplate.replace('vidsrc', url);
+        }
+    } else {
+        console.error('VideoPopup size is not a string or an object. It can be a string (ex. "medium"), or an object (ex. {width:512, height:288})');
+    }
 }
 
-document.getElementById('popupvid').addEventListener('click', VideoPopup('C:\\Users\\DJRco_000\\Videos\\Roblox\\robloxapp-20170828-1334197.wmv'));
